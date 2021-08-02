706b65f521f05c5d01bdca4177c42aa9e32c8943
Philipp Dewald <s8phdewa@stud.uni-saarland.de>
2021-08-02 10:49:29 +0200

    output

--- cloud-detection.py
+++ cloud-detection.py
@@ -12,6 +12,9 @@ from cryptography.x509.oid import NameOID
 import idna
 from socket import socket
 
+from colorama import Fore
+from colorama import Style
+
 logging.basicConfig(format='', level=logging.INFO)
 
 class Detector:
@@ -34,8 +37,13 @@ class Detector:
 		self.AzureASN = ['8068', '8069', '8070', '8071', '8072', '8073', '8074', '8075']
 		self.AWSASN = ['7224', '14618', '16509', '17493', '10124', '9059']
 
+		self.Azure_use_case = []
+		self.AWS_use_case = []
+		self.Azure_links = []
+		self.AWS_links = []
 
-	def checkForCloudService(self, data):
+
+	def checkForCloudService(self, data, use_case):
 
 		AzureKeywords = ['azure', 'Azure', 'core.windows.net', 'azurewebsites.net']
 		AWSKeywords = ['aws', 's3-website', 'S3', 'EC2', 'ECS']
@@ -44,9 +52,15 @@ class Detector:
 			# don't go for 'Microsoft'!
 			if "azure" in str(item) or "core.windows.net" in str(item) or "azurewebsites.net" in str(item) or "Azure" in str(item):
 				self.Azure = True
+				if 'http' in str(item):
+					self.Azure_links.append(str(item))
+				self.Azure_use_case.append(use_case)
 			# don't go for 'Amazon'!
 			if "aws" in str(item) or "s3-website" in str(item) or "S3" in str(item) or "EC2" in str(item) or "ECS" in str(item):	
 				self.AWS = True
+				if 'http' in str(item):
+					self.AWS_links.append(str(item))
+				self.AWS_use_case.append(use_case)
 			if "MS=ms" in str(item) or "outlook" in str(item):
 				self.Office365 = True
 			if "ZOOM_verify" in str(item):
@@ -64,7 +78,7 @@ class Detector:
 		try:
 			for nameserver in dns.resolver.resolve(self.domain,'NS'):
 				nameservers.append(nameserver)
-			self.checkForCloudService(nameservers)
+			self.checkForCloudService(nameservers, 'nameserver')
 		except:
 			pass
 
@@ -73,7 +87,7 @@ class Detector:
 		try:
 			for txt in dns.resolver.resolve(self.domain,'TXT'):
 				txt_records.append(txt)
-			self.checkForCloudService(txt_records)
+			self.checkForCloudService(txt_records, 'TXT records')
 		except:
 			pass	
 
@@ -81,7 +95,7 @@ class Detector:
 			mx_records = []
 			for mx in dns.resolver.resolve(self.domain,'MX'):
 				mx_records.append(mx)
-			self.checkForCloudService(mx_records)
+			self.checkForCloudService(mx_records, 'MX records')
 		except:
 			pass
 
@@ -111,9 +125,12 @@ class Detector:
 	    try:
 	        names = self.crypto_cert.issuer.get_attributes_for_oid(NameOID.COMMON_NAME)
 	        self.cert_issuer = names[0].value
-	        print(self.cert_issuer)
-	        if "Amazon" in self.cert_issuer: self.AWS = True
-	        if "Microsoft" in self.cert_issuer: self.Azure = True
+	        if "Amazon" in self.cert_issuer: 
+	        	self.AWS = True
+	        	self.AWS_use_case.append('website certificate')
+	        if "Microsoft" in self.cert_issuer: 
+	        	self.Azure = True
+	        	self.Azure_use_case.append('website certificate')
 	    except x509.ExtensionNotFound:
 	        return #None	    
 
@@ -136,7 +153,7 @@ class Detector:
 			if found_url not in self.found_urls:
 				self.found_urls.append(found_url)
 
-		self.checkForCloudService(self.found_urls)
+		self.checkForCloudService(self.found_urls, 'linked website from cloud provider')
 
 	def checkAutonmousSystem(self):
 		# done via querying https://hackertarget.com since there is no handy python library
@@ -148,8 +165,10 @@ class Detector:
 
 		if ASN in self.AzureASN:
 			self.Azure = True
+			self.Azure_use_case.append('Autonomous System')
 		if ASN in self.AWSASN:
 			self.AWS = True
+			self.AWS_use_case.append('Autonomous System')
 
 	def flAWS_cloud(self):
 		# implementation of some useful tools from http://flaws.cloud/
@@ -164,16 +183,20 @@ class Detector:
 				pass
 
 	def checkServer(self):
-		url = r.get('http://' + self.domain).url
-		server = r.get(url).headers['Server']
-		self.checkForCloudService([server])
+		try:
+			url = r.get('http://' + self.domain).url
+			server = r.get(url).headers['Server']
+			self.checkForCloudService([server], 'name of the server')
+		except:
+			return
 
 	#def detectInHTML(self): maybe cloudbrute as inspiration
 
 
 	def run(self):
+		logging.info(f'{Fore.YELLOW}Checking: {self.domain}{Style.RESET_ALL}')
 		self.getIP()
-		self.checkForCloudService([self.domain])
+		self.checkForCloudService([self.domain], 'domain name')
 		self.getNameservers()
 		self.checkFurtherDNSEntries()
 		# check certificate only if one exists:
@@ -185,12 +208,22 @@ class Detector:
 		self.flAWS_cloud()
 		self.checkServer()
 
-		if self.Azure: logging.info(f'Detected: Microsoft Azure')
-		if self.AWS: logging.info(f'Detected: Amazon Web Services')
-		if self.Office365: logging.info(f'Detected: Office365')
-		if self.Zoom: logging.info(f'Detected: Zoom')
-		if self.Dropbox: logging.info(f'Detected: Dropbox')
+		self.Azure_use_case = list(dict.fromkeys(self.Azure_use_case))
+		self.AWS_use_case = list(dict.fromkeys(self.AWS_use_case))
 
+		no_output = True
+
+		if self.Azure: logging.info(f'{Fore.RED}Detected:{Style.RESET_ALL} Microsoft Azure: identified via {", ".join(self.Azure_use_case)}'); no_output = False
+		if self.Azure_links: logging.info(f'{Fore.CYAN}Azure links:{Style.RESET_ALL} {", ".join(self.Azure_links)}'); no_output = False
+
+		if self.AWS: logging.info(f'{Fore.RED}Detected:{Style.RESET_ALL} Amazon Web Services: identified via {", ".join(self.AWS_use_case)}'); no_output = False
+		if self.AWS_links: logging.info(f'{Fore.CYAN}AWS links:{Style.RESET_ALL} {", ".join(self.AWS_links)}'); no_output = False
+		
+		if self.Office365: logging.info(f'{Fore.RED}Detected:{Style.RESET_ALL} Office365'); no_output = False
+		if self.Zoom: logging.info(f'{Fore.RED}Detected:{Style.RESET_ALL} Zoom'); no_output = False
+		if self.Dropbox: logging.info(f'{Fore.RED}Detected:{Style.RESET_ALL} Dropbox'); no_output = False
+
+		if no_output: logging.info(f'{Fore.YELLOW}Nothing detected{Style.RESET_ALL}')
 
 if __name__ == "__main__":
 	Detector("ais-security.de").run()
