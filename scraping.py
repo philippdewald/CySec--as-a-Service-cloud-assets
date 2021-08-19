04fd8f0af9852d367f523bcf2d600398da90901b
Philipp Dewald <s8phdewa@stud.uni-saarland.de>
2021-08-19 18:47:03 +0200

    checking for company identifiers on the websites

--- scraping.py
+++ scraping.py
@@ -1,5 +1,7 @@
 import requests
 import urllib
+from urllib.parse import urlparse
+from urllib.parse import urljoin
 import pandas as pd 
 from requests_html import HTML
 from requests_html import HTMLSession
@@ -7,9 +9,12 @@ from requests_html import HTMLSession
 import pprint as p
 import time
 from bs4 import BeautifulSoup
+from bs4.element import Comment
 
 from colorama import Fore
 from colorama import Style
+
+import re
 
 
 def get_source(url):
@@ -74,9 +79,14 @@ def scrape(query):
 def get_associated_websites(links):
     associated_websites = []
     potential_associated_websites = []
+    company_information_fields = ['privacy', 'policy', 'contact', 'copyright', 'imprint',  
+                                  'terms', 'conditions', 'support']
+
     for link in links:
         if link.endswith('.pdf'):
             continue
+
+        domain = urlparse(link).netloc
 
         # handle websites that can't be reached
         try:
@@ -86,28 +96,68 @@ def get_associated_websites(links):
 
         soup = BeautifulSoup(html, 'html.parser')
 
-        # look for links
-        for item in soup.findAll('a'):
+        #print(f'{Fore.RED}Current Link is: {link}{Style.RESET_ALL}')
+
+        found_hrefs = []
+        for item in soup.findAll('a'):  # every iteration is a href of link
             href = item.get('href')
+
             if href is None or len(href) == 0:
                 continue
 
-            # see if there is a link to the main website
-            if main_domain in href:
-                if link not in associated_websites:
-                    associated_websites.append(link)
-                continue
+            # only look into new href's
+            if href not in found_hrefs:
+                found_hrefs.append(href)
 
-            # see if the companyname is in linked urls
-            if companyname in href:
-                if link not in associated_websites and link not in potential_associated_websites:
-                    potential_associated_websites.append(link)
+                # 1. see if there is a link to the main website
+                if main_domain in href:
+                    if link not in associated_websites:
+                        associated_websites.append(link)
+                    continue
 
+                # 2. see if the companyname is in linked urls
+                if companyname in href:
+                    if link not in associated_websites and link not in potential_associated_websites:
+                        potential_associated_websites.append(link)
+                
+                # 3. see if we get some information out of copyright text, terms of service, privacy policy text, ...
+                if any(key in href for key in company_information_fields):
+                    # visit href and see if companyname is included
+                    # if yes, add it to associated_websites, it not, delete it when it appears in 
+                    # associated_websites or potential_associated_websites and continue with next link
+
+                    # make abolute links
+                    if not href.startswith('http'):
+                        url = requests.get('http://' + domain).url
+                        href = urljoin(url, href)
+                        if domain not in href:
+                            continue
+
+                    try:
+                        text = requests.get(href).text
+                    except:
+                        continue
+
+                    company_information_soup = BeautifulSoup(text, 'html.parser')
+                    texts = company_information_soup.findAll(text=True)
+                    visible_texts = filter(tag_visible, texts)
+
+                    companyname_regex = re.compile(companyname, re.MULTILINE | re.IGNORECASE)
+                    if re.search(companyname_regex, u" ".join(t.strip() for t in visible_texts)):
+                        #print('Get the champagne out ' + href, link)                                   # for debugging
+                        if link not in associated_websites:
+                            associated_websites.append(link)
+                        break
+                    else:
+                        #print('We dont want wrong sites: ' + href, link)                               # for debugging
+                        if link in potential_associated_websites:
+                            potential_associated_websites.remove(link)
+                        if link in associated_websites:
+                            associated_websites.remove(link)
+                            potential_associated_websites.append(link)      # necessary? probably not, but maybe there was a mistake so save it here
+                        break 
         # look for website content
         #TODO
-        # see if we get some information out of copyright text, terms of service, privacy policy text
-
-
 
     if associated_websites:
         print(f'{Fore.RED}We\'ve got them! Their associated websites are as follows:{Style.RESET_ALL}')
@@ -120,6 +170,14 @@ def get_associated_websites(links):
     if not associated_websites and not potential_associated_websites:
         print('Nothing detected here.') 
 
+
+# from https://stackoverflow.com/questions/1936466/beautifulsoup-grab-visible-webpage-text
+def tag_visible(element):
+    if element.parent.name in ['style', 'script', 'head', 'title', 'meta', '[document]']:
+        return False
+    if isinstance(element, Comment):
+        return False
+    return True
 
 """
 In case of 429, try:
@@ -144,7 +202,7 @@ if keyword:
     scrape("site:s3.amazonaws.com -filetype:pdf " + companyname + " " + keyword)
 else:
     scrape("site:s3.amazonaws.com -filetype:pdf " + companyname)
-    
+
 print(f'{Fore.CYAN}Checking Azure websites{Style.RESET_ALL}')
 if keyword:
     scrape("site:azurewebsites.net -filetype:pdf " + companyname + " " + keyword)
